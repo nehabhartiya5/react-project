32c981e4f2d9f6f87e1d31d2bad37276fb4d6fcf
--global <--global>
2025-05-11 13:38:17 +0530

    react project code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom';
+
+import Devbar from '@/components/Devbar/Devbar';
+
+const App = () => {
+  return (
+    <>
+      <div className='fixed bottom-0 left-0 top-0'>
+        <Devbar />
+      </div>
+      <div className='ml-[700px]'>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default App;
